68493676ebe5fd4928a156acd2e25ee782bc75e4
toremann <50668920+toremann@users.noreply.github.com>
2022-08-30 22:03:21 +0200

    prepare for mongodb

---
+++ pages/api/certs.js
@@ -0,0 +1,10 @@
+import { connectToDatabase } from '../../util/mongodb';
+
+export default async (req, res) => {
+    const { db } = await connectToDatabase();
+    console.log(res);
+
+    const certs = await db.collection('certs').find({}).toArray();
+
+    res.json(certs);
+};
