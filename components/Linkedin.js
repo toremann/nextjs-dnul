415e41f6987d408c6d0d0699570b40b1dbeed7aa
toremann <50668920+toremann@users.noreply.github.com>
2022-09-10 13:05:24 +0200

    refactor
    Co-Authored-By: showerbeer <12481167+showerbeer@users.noreply.github.com>

--- components/Linkedin.js
+++ components/Linkedin.js
@@ -4,32 +4,39 @@ import styles from '../styles/Home.module.css';
 import { AiOutlineLinkedin } from 'react-icons/ai';
 
 const Linkedin = ({ certs }) => {
-    return (
+  return (
+    <>
+      {certs.length === 0 ? (
+        <span> no data soz</span>
+      ) : (
         <>
-            <motion.p className={styles.description} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
-                {' '}
-                Recent certificates:{' '}
-            </motion.p>
-            <motion.div className={styles.grid} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
-                {certs.map((certs) => {
-                    return (
-                        <div key={certs._id} className={styles.card}>
-                            <div className={styles.card__content}>
-                                <a href={certs.url}>
-                                    <b>{certs.course}</b>
-                                </a>
-                                <br />
-                                By: <i>{certs.author}</i>
-                            </div>
-                            <div className={styles.card__type}>
-                                <AiOutlineLinkedin size={30} />
-                            </div>
-                        </div>
-                    );
-                })}
-            </motion.div>
+          {' '}
+          <motion.p className={styles.description} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+            {' '}
+            Recent certificates:{' '}
+          </motion.p>
+          <motion.div className={styles.grid} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+            {certs.map((certs) => {
+              return (
+                <div key={certs._id} className={styles.card}>
+                  <div className={styles.card__content}>
+                    <a href={certs.url}>
+                      <b>{certs.course}</b>
+                    </a>
+                    <br />
+                    By: <i>{certs.author}</i>
+                  </div>
+                  <div className={styles.card__type}>
+                    <AiOutlineLinkedin size={30} />
+                  </div>
+                </div>
+              );
+            })}
+          </motion.div>
         </>
-    );
+      )}
+    </>
+  );
 };
 
 export default Linkedin;
--- components/Logo.js
+++ components/Logo.js
@@ -3,29 +3,29 @@ import styles from '../styles/Home.module.css';
 import { motion } from 'framer-motion';
 
 const Logo = () => {
-    const username = 'toremann';
-    const dnul = 'Dnul';
+  const username = 'toremann';
+  const dnul = 'Dnul';
 
-    return (
-        <div className={styles.logo}>
-            <motion.h1 className={styles.title__left} initial={{ x: '-100vw' }} animate={{ x: 0 }} transition={{ type: 'spring', duration: 1, bounce: 0.3 }}>
-                Hi, I`m{' '}
-            </motion.h1>
-            <motion.h1
-                className={styles.title__right}
-                initial={{ y: '-200vw' }}
-                animate={{ y: 0 }}
-                transition={{
-                    type: 'spring',
-                    duration: 1,
-                    bounce: 0.3,
-                    delay: 0.6,
-                }}
-            >
-                <a href={`https://github.com/${username}`}>{dnul}</a>
-            </motion.h1>
-        </div>
-    );
+  return (
+    <div className={styles.logo}>
+      <motion.h1 className={styles.title__left} initial={{ x: '-100vw' }} animate={{ x: 0 }} transition={{ type: 'spring', duration: 1, bounce: 0.3 }}>
+        Hi, I`m{' '}
+      </motion.h1>
+      <motion.h1
+        className={styles.title__right}
+        initial={{ y: '-200vw' }}
+        animate={{ y: 0 }}
+        transition={{
+          type: 'spring',
+          duration: 1,
+          bounce: 0.3,
+          delay: 0.6,
+        }}
+      >
+        <a href={`https://github.com/${username}`}>{dnul}</a>
+      </motion.h1>
+    </div>
+  );
 };
 
 export default Logo;
